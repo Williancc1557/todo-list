008ebb9fdedf466c987da04623f9135ab853c6ea
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2023-10-06 00:13:00 -0300

    feat: make loading page for home

--- src/app/services/loading-interceptor.ts
+++ src/app/services/loading-interceptor.ts
@@ -7,20 +7,25 @@ import {
 import { Observable, finalize } from 'rxjs';
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
+import { TaskService } from './task.service';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    public readonly taskService: TaskService
+  ) {}
 
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    console.log('entrou aq');
-
+    this.taskService.isLoading = true;
     this.authService.isLoading = true;
+
     return next.handle(req).pipe(
       finalize(() => {
+        this.taskService.isLoading = false;
         this.authService.isLoading = false;
       })
     );
--- src/app/views/todo.component.ts
+++ src/app/views/todo.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { TaskService } from '../services/task.service';
 
 @Component({
+  styleUrls: ['./todo.component.css'],
   selector: 'todo-app',
   templateUrl: './todo.component.html',
 })
-export class TodoListComponent {}
+export class TodoListComponent {
+  constructor(public readonly taskService: TaskService) {}
+}
